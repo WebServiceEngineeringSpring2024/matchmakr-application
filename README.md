7dc9ff48568cf3c25c7d8d22a7a91f8069fe0e0b
reedmccallum <14640761+reedmccallum@users.noreply.github.com>
2024-03-03 23:29:52 -0500

    refactor WebServicesEngFE -> matchmakr-application

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WebServiceEngFE
+# matchmakr-application
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-service-eng-fe'),
+      dir: require('path').join(__dirname, './coverage/matchmakr-application'),
       subdir: '.',
       reporters: [
         { type: 'html' },
