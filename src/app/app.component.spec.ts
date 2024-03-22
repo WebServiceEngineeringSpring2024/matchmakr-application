4b6f9a06e4a0039f836f7107a2f96161516c899e
jblake33 <jcb1354@gmail.com>
2024-03-22 14:42:57 -0400

    push overhaul

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Matchmakr' title`, () => {
+  it(`should have the 'matchmakr-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Matchmakr');
+    expect(app.title).toEqual('matchmakr-app');
   });
 
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, matchmakr-app');
+  });
 });
