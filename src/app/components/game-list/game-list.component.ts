43ac8c977e2537cd832ce9509455fc0a504d4d2b
jblake33 <jcb1354@gmail.com>
2024-03-26 13:15:48 -0400

    reorganize components, add search

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -1,32 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {GameService} from '../../services/game.service';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [NgFor, FormsModule],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './game-list.component.html',
   styleUrl: './game-list.component.css'
 })
 export class GameListComponent implements OnInit {
   games: Game[];
+  filterGames: Game[];
   query: string;
+  isFiltered: boolean;
   constructor(private gss: GameService) {
     this.query = "";
     this.games = [];
+    this.filterGames = [];
+    this.isFiltered = false;
   }
+  // Runs once the page is initialized
   ngOnInit(): void {
     this.getGames();
   }
+  // Call to the game service to get a list of all games 
   private getGames() {
     this.gss.getAllGames().subscribe(data => {
       this.games = data;
     })
   }
+  // Submit search query
   Submit(filter: string) {
-    alert(filter);
+    this.filterGames = [];
+    this.isFiltered = true;
+    // perform a case insensitive search on the data in the games list
+    this.games.forEach(game => {
+      if (game.name.toLowerCase().includes(filter.toLowerCase())) {
+        this.filterGames.push(game);
+      }
+    });
+    // if no games were found, alert user 
+    if (this.filterGames.length == 0) {
+      alert("No games found containing the name " + filter);
+    }
+  }
+  Clear() {
+    this.isFiltered = false;
   }
 }
